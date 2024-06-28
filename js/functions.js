43613b92982334c02233fdb8d662306100e79a83
1187325506@qq.com <1187325506@qq.com>
2024-06-28 16:43:23 +0800

    fix some bugs

---
+++ js/functions.js
@@ -0,0 +1,156 @@
+﻿
+var $window = $(window), gardenCtx, gardenCanvas, $garden, garden;
+var clientWidth = $(window).width();
+var clientHeight = $(window).height();
+
+$(function () {
+	// setup garden
+	$loveHeart = $("#loveHeart");
+	var offsetX = $loveHeart.width() / 2;
+	var offsetY = $loveHeart.height() / 2 - 55;
+	$garden = $("#garden");
+	gardenCanvas = $garden[0];
+	gardenCanvas.width = $("#loveHeart").width();
+	gardenCanvas.height = $("#loveHeart").height()
+	gardenCtx = gardenCanvas.getContext("2d");
+	gardenCtx.globalCompositeOperation = "lighter";
+	garden = new Garden(gardenCtx, gardenCanvas);
+
+	$("#content").css("width", $loveHeart.width() + $("#code").width());
+	$("#content").css("height", Math.max($loveHeart.height(), $("#code").height()));
+	$("#content").css("margin-top", Math.max(($window.height() - $("#content").height()) / 2, 10));
+	$("#content").css("margin-left", Math.max(($window.width() - $("#content").width()) / 2, 10));
+
+	// renderLoop
+	setInterval(function () {
+		garden.render();
+	}, Garden.options.growSpeed);
+});
+
+$(window).resize(function () {
+	var newWidth = $(window).width();
+	var newHeight = $(window).height();
+	if (newWidth != clientWidth && newHeight != clientHeight) {
+		location.replace(location);
+	}
+});
+
+function getHeartPoint(angle) {
+	var t = angle / Math.PI;
+	var x = 19.5 * (16 * Math.pow(Math.sin(t), 3));
+	var y = - 20 * (13 * Math.cos(t) - 5 * Math.cos(2 * t) - 2 * Math.cos(3 * t) - Math.cos(4 * t));
+	return new Array(offsetX + x, offsetY + y);
+}
+
+function startHeartAnimation() {
+	var interval = 50;
+	var angle = 10;
+	var heart = new Array();
+	var animationTimer = setInterval(function () {
+		var bloom = getHeartPoint(angle);
+		var draw = true;
+		for (var i = 0; i < heart.length; i++) {
+			var p = heart[i];
+			var distance = Math.sqrt(Math.pow(p[0] - bloom[0], 2) + Math.pow(p[1] - bloom[1], 2));
+			if (distance < Garden.options.bloomRadius.max * 1.3) {
+				draw = false;
+				break;
+			}
+		}
+		if (draw) {
+			heart.push(bloom);
+			garden.createRandomBloom(bloom[0], bloom[1]);
+		}
+		if (angle >= 30) {
+			clearInterval(animationTimer);
+			showMessages();
+		} else {
+			angle += 0.2;
+		}
+	}, interval);
+}
+
+(function ($) {
+	$.fn.typewriter = function () {
+		this.each(function () {
+			var $ele = $(this), str = $ele.html(), progress = 0;
+			$ele.html('');
+			var timer = setInterval(function () {
+				var current = str.substr(progress, 1);
+				if (current == '<') {
+					progress = str.indexOf('>', progress) + 1;
+				} else {
+					progress++;
+				}
+				$ele.html(str.substring(0, progress) + (progress & 1 ? '_' : ''));
+				if (progress >= str.length) {
+					clearInterval(timer);
+				}
+			}, 75);
+		});
+		return this;
+	};
+})(jQuery);
+//时间选择器
+function timeElapse(birthday) {
+	// var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
+	// console.log(nowDay)
+	// console.log(birthday)
+	// var seconds = (nowDay - birthday) / 1000;
+	// console.log(seconds)
+	// var days = Math.floor(seconds / (3600 * 24));
+	// seconds = seconds % (3600 * 24);
+	// var hours = Math.floor(seconds / 3600);
+	// if (hours < 10) {
+	// 	hours = "0" + hours;
+	// }
+	// seconds = seconds % 3600;
+	// var minutes = Math.floor(seconds / 60);
+	// if (minutes < 10) {
+	// 	minutes = "0" + minutes;
+	// }
+	// seconds = seconds % 60;
+	// if (seconds < 10) {
+	// 	seconds = "0" + seconds;
+	// }
+
+	var date1=new Date(); //开始时间，当前时间
+	var date2=new Date("2024/06/22"); //结束时间，需传入时间参数
+	var date3=date1.getTime()-date2.getTime(); //时间差的毫秒数
+	//计算出相差天数
+	var days=Math.floor(date3/(24*3600*1000));
+	//计算出小时数
+	var leave1=date3%(24*3600*1000); //计算天数后剩余的毫秒数
+	var hours=Math.floor(leave1/(3600*1000));
+	//计算相差分钟数
+	var leave2=leave1%(3600*1000); //计算小时数后剩余的毫秒数
+	var minutes=Math.floor(leave2/(60*1000));
+	//计算相差秒数
+	var leave3=leave2%(60*1000); //计算分钟数后剩余的毫秒数
+	var seconds=Math.round(leave3/1000);
+	console.log(" 相差 "+days+"天 "+hours+"小时 "+minutes+" 分钟"+seconds+" 秒");
+
+	var result = "<span class=\"digit\">" + days + "</span> days <span class=\"digit\">" + hours + "</span> hours <span class=\"digit\">" + minutes + "</span> minutes <span class=\"digit\">" + seconds + "</span> seconds";
+	$("#elapseClock").html(result);
+}
+
+function showMessages() {
+	adjustWordsPosition();
+	$('#messages').fadeIn(5000, function () {
+		showLoveU();
+	});
+}
+
+function adjustWordsPosition() {
+	$('#words').css("position", "absolute");
+	$('#words').css("top", $("#garden").position().top + 195);
+	$('#words').css("left", $("#garden").position().left + 70);
+}
+
+function adjustCodePosition() {
+	$('#code').css("margin-top", ($("#garden").height() - $("#code").height()) / 2);
+}
+
+function showLoveU() {
+	$('#loveu').fadeIn(3000);
+}
